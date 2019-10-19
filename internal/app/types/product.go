d84c90b49c4f113627092297c14b4f74c256688a
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2019-10-19 18:26:45 +0700

    refactor models into types, and move them separated to each file

---
+++ internal/app/types/product.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Sku             string `gorm:"unique_index;not_null"`
+	Name            string `gorm:"not_null;index:name"`
+	CurrentQuantity int    `gorm:"not_null"`
+}
