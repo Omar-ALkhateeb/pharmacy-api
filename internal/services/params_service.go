37327d0016496d3a129a93e42d31d9aa5a55bf32
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2019-10-20 22:25:03 +0700

    params service fix DecideDate
    This in order to allow params to specify either, not both

--- internal/services/params_service.go
+++ internal/services/params_service.go
@@ -8,11 +8,15 @@ func DecideDate(c *gin.Context) (string, string) {
 	var decidedStartDate string
 	var decidedEndDate string
 
-	if len(queryParams) == 0 {
+	if len(queryParams["start_date"]) == 0 {
 		decidedStartDate = "1990-01-01"
-		decidedEndDate = "4000-12-12"
 	} else {
 		decidedStartDate = queryParams["start_date"][0]
+	}
+
+	if len(queryParams["end_date"]) == 0 {
+		decidedEndDate = "4000-12-12"
+	} else {
 		decidedEndDate = queryParams["end_date"][0]
 	}
 
