3906ef7ddbdf964879e9801b1e706d14d4e6d19b
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2019-10-20 10:27:50 +0700

    create stock in, validate quantities
    received quantity cannot be more than received quantity

--- internal/app/handlers/stock_ins_handlers.go
+++ internal/app/handlers/stock_ins_handlers.go
@@ -8,10 +8,14 @@ import (
 	"time"
 )
 
-// First find if product by such SKU exists.
+// First check if received quantity > ordered quantity.
+// If yes, return 422.
+//
+// Second find if product by such SKU exists.
 // If not return response 422.
 //
 // If product already exists, add the quantity.
+// And create stock in data.
 func CreateStockIn(c *gin.Context) {
 	db := database.DBConn
 	var responseCode int
@@ -19,6 +23,11 @@ func CreateStockIn(c *gin.Context) {
 	var StockInParams paramstypes.StockInCreateWithSku
 
 	if c.ShouldBind(&StockInParams) == nil {
+		if StockInParams.ReceivedQuantity > StockInParams.OrderedQuantity {
+			c.String(422, "Received Quantity cannot be more than Ordered Quantity")
+			return
+		}
+
 		var product types.Product
 		if err := db.Where("sku = ?", StockInParams.Sku).First(&product).Error; err != nil {
 			c.String(422, "Product does not exist")
